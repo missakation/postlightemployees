0f781479bf1ff212f3bf6d0c9c3b2ef8c0308de2
MissakBoyajian <missakboya@live.com>
2019-07-27 17:54:29 +0300

    connecting employees table to API calls

--- Client/src/EmployeeDetails/EmployeeDetails.js
+++ Client/src/EmployeeDetails/EmployeeDetails.js
@@ -28,6 +28,7 @@ const useStyles = makeStyles(theme => ({
     }
 
 }));
+
 export default function Employees() {
 
 
--- Client/src/Employees/Employees.js
+++ Client/src/Employees/Employees.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { render } from "react-dom"
-
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -8,6 +6,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
+import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
@@ -15,19 +14,42 @@ import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
+import TextField from '@material-ui/core/TextField';
 
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from '@material-ui/icons/Add';
 
-const useStyles1 = makeStyles(theme => ({
+import axios from 'axios';
+import { navigate } from "@reach/router"
+
+const employeeStyles = makeStyles(theme => ({
     root: {
         flexShrink: 0,
         color: theme.palette.text.secondary,
         marginLeft: theme.spacing(2.5),
     },
+    addicon: {
+        fontSize: 30
+    },
+    leftIcon: {
+        marginRight: theme.spacing(1),
+    },
+    rightIcon: {
+        marginLeft: theme.spacing(1),
+    },
+    iconSmall: {
+        fontSize: 20,
+    },
 }));
 
 
+
+
+
 function TablePaginationActions(props) {
-    const classes = useStyles1();
+    const classes = employeeStyles();
     const theme = useTheme();
     const { count, page, rowsPerPage, onChangePage } = props;
 
@@ -84,26 +106,6 @@ TablePaginationActions.propTypes = {
     rowsPerPage: PropTypes.number.isRequired,
 };
 
-function createData(name, calories, fat) {
-    return { name, calories, fat };
-}
-
-const employees = [
-    createData('Cupcake', 305, 3.7),
-    createData('Donut', 452, 25.0),
-    createData('Eclair', 262, 16.0),
-    createData('Frozen yoghurt', 159, 6.0),
-    createData('Gingerbread', 356, 16.0),
-    createData('Honeycomb', 408, 3.2),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Jelly Bean', 375, 0.0),
-    createData('KitKat', 518, 26.0),
-    createData('Lollipop', 392, 0.2),
-    createData('Marshmallow', 318, 0),
-    createData('Nougat', 360, 19.0),
-    createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
-
 const useStyles2 = makeStyles(theme => ({
     root: {
         width: '100%',
@@ -121,11 +123,13 @@ function Welcome(props) {
     const classes = useStyles2();
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [employees, setEmployees] = useState([]);
+    const [searchCriteria, setsearchCriteria] = useState('');
 
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, employees.length - page * rowsPerPage);
 
     function handleChangePage(event, newPage) {
-        setPage(newPage);
+        getEmployees(newPage);
     }
 
     function handleChangeRowsPerPage(event) {
@@ -133,48 +137,103 @@ function Welcome(props) {
         setPage(0);
     }
 
-    return (
-        <Paper className={classes.root}>
-            <div className={classes.tableWrapper}>
-                <Table className={classes.table}>
-                    <TableBody>
-                        {employees.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
-                            <TableRow key={row.name}>
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                            </TableRow>
-                        ))}
+    function openAddEmployeePage(event) {
+        navigate('/employeedetails');
+    }
 
-                        {emptyRows > 0 && (
-                            <TableRow style={{ height: 48 * emptyRows }}>
-                                <TableCell colSpan={6} />
-                            </TableRow>
-                        )}
-                    </TableBody>
-                    <TableFooter>
-                        <TableRow>
-                            <TablePagination
-                                rowsPerPageOptions={[5, 10, 25]}
-                                colSpan={3}
-                                count={employees.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                SelectProps={{
-                                    inputProps: { 'aria-label': 'employees per page' },
-                                    native: true,
-                                }}
-                                onChangePage={handleChangePage}
-                                onChangeRowsPerPage={handleChangeRowsPerPage}
-                                ActionsComponent={TablePaginationActions}
-                            />
-                        </TableRow>
-                    </TableFooter>
-                </Table>
+    function getEmployees(page) {
+
+        axios.get("http://localhost:3000/api/employees", {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('postlight-token')
+            }
+        }).then(res => {
+
+            setEmployees(res.data.data);
+
+        }).catch(error => {
+
+        })
+    }
+
+    useEffect(() => {
+        // getEmployees(1);
+    });
+
+
+    return (
+        <div className="page">
+            <div className="emp-sec-options">
+                <TextField
+                    id="outlined-search"
+                    label="Search Employee"
+                    type="search"
+                    className={classes.textField}
+                    margin="normal"
+                    variant="outlined"
+                />
+
+                <Button variant="contained" color="primary" className={classes.button} onClick={openAddEmployeePage}>
+                    Add
+                <AddIcon className={classes.rightIcon} />
+                </Button>
             </div>
-        </Paper>
+
+            <Paper className={classes.root}>
+                <div className={classes.tableWrapper}>
+                    <Table className={classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell align="left">Name</TableCell>
+                                <TableCell align="left">Title</TableCell>
+                                <TableCell align="right">Location</TableCell>
+                                <TableCell align="right">Department</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {employees.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+                                <TableRow key={row.name}>
+                                    <TableCell></TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right"></TableCell>
+                                </TableRow>
+                            ))}
+
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: 48 * emptyRows }}>
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    rowsPerPageOptions={[10, 25, 50]}
+                                    colSpan={6}
+                                    count={employees.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    SelectProps={{
+                                        inputProps: { 'aria-label': 'employees per page' },
+                                        native: true,
+                                    }}
+                                    onChangePage={handleChangePage}
+                                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                                    ActionsComponent={TablePaginationActions}
+                                />
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </div>
+            </Paper>
+        </div>
     );
 }
 
